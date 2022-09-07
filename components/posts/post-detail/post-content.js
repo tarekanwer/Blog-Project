98f7b91994c1978e3adf38b23929dfde6f6c3a67
tarekanwer <tito.a7mad@gmail.com>
2022-09-08 00:08:10 +0200

    replacing traditional react mark down elements

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,18 +1,47 @@
-import ReactMarkdown from 'react-markdown';
-import classes from './post-content.module.css';
-import PostHeader from './post-header';
-
+import ReactMarkdown from "react-markdown";
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+import Image from "next/image";
 
 const PostContent = (props) => {
   const post = props.post;
-    const imgPath = `/images/posts/${post.slug}/${post.image}`;
-  return (
-    <article  className={classes.content}>
-        <PostHeader title = {post.title} image = {imgPath} />
-        <ReactMarkdown>{post.content}</ReactMarkdown>
-        
-    </article>
-  )
-}
+  const imgPath = `/images/posts/${post.slug}/${post.image}`;
 
-export default PostContent
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imgPath} />
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
+};
+
+export default PostContent;
