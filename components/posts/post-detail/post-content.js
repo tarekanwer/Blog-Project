4999d154b752e9d76fd7ed312304e4a98c861cd3
tarekanwer <tito.a7mad@gmail.com>
2022-09-07 23:27:09 +0200

    fetching for dynamic paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,20 +2,14 @@ import ReactMarkdown from 'react-markdown';
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
 
-const dummyPost = {
-    slug: "getting-started-with-nextjs",
-    title: "Getting started with NextJS",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    content : '# This is a first post'
-  }
 
-const PostContent = () => {
-    const imgPath = `/images/posts/${dummyPost.slug}/${dummyPost.image}`
+const PostContent = (props) => {
+  const post = props.post;
+    const imgPath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article  className={classes.content}>
-        <PostHeader title = {dummyPost.title} image = {imgPath} />
-        <ReactMarkdown>{dummyPost.content}</ReactMarkdown>
+        <PostHeader title = {post.title} image = {imgPath} />
+        <ReactMarkdown>{post.content}</ReactMarkdown>
         
     </article>
   )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,30 @@
 import PostContent from "../../components/posts/post-detail/post-content";
-const PostDetailPage = () => {
-  return <PostContent />;
+import { getPostData , getPostsFiles } from "../../lib/posts-util";
+
+const PostDetailPage = (props) => {
+  return <PostContent post = {props.post} />;
 };
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const paths = getPostsFiles();
+  const slugs = paths.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
 
 export default PostDetailPage;
