46c66aa92edbdbd1ce85d4d6f223aa7987e8bebe
tarekanwer <tito.a7mad@gmail.com>
2022-09-08 19:13:03 +0200

    adding heads to improve SEO

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
 import ContactForm from "../components/contact/contact-form";
+import { Fragment } from "react";
+import Head from "next/head";
 
 const ContactPage = () => {
-    return <ContactForm />
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me !</title>
+        <meta name="description" content="send me your messages" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 };
 
 export default ContactPage;
