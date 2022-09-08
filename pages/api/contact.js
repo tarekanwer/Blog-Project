8530ee0eea19be6804f3338bca546c2989b1e027
tarekanwer <tito.a7mad@gmail.com>
2022-09-08 05:51:13 +0200

    storing data in the mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -21,16 +23,38 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+
+    try {
+       client = await MongoClient.connect(
+        "mongodb+srv://tarekanwer:etpBXzOb7NuOz2Au@cluster0.hvrekjg.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({
+        message: "could not connect to database",
+      });
+      return;
+    }
+
+    const db = client.db();
+    let result;
+    try {
+      result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({
+        message: "storing message failed",
+      });
+    }
+
+    client.close();
+
     res.status(201).json({
-        message : "Successfully stored message",
-        message : newMessage
-    })
+      message: "Successfully stored message",
+      message: newMessage,
+    });
   }
-
-
-
-
 }
 
 export default handler;
