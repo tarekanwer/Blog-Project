074a4cbf4886a2c3b42ed837a284ff56463eed7e
tarekanwer <tito.a7mad@gmail.com>
2022-09-09 13:16:48 +0200

    optimizing db for production

--- next.config.js
+++ next.config.js
@@ -1,12 +1,43 @@
-module.exports = {
-    webpack: (config, { isServer }) => {
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      webpack: (config, { isServer }) => {
         if (!isServer) {
-            // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-            config.node = {
-                fs: 'empty'
-            }
+          // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+          config.node = {
+            fs: "empty",
+          };
         }
 
         return config;
-    }
-}
+      },
+      env: {
+        mongodb_username: "tarekanwer",
+        mongodb_password: "etpBXzOb7NuOz2Au",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+
+  return {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.node = {
+          fs: "empty",
+        };
+      }
+
+      return config;
+    },
+    env: {
+      mongodb_username: "tarekanwer",
+      mongodb_password: "etpBXzOb7NuOz2Au",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  };
+};
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -25,11 +25,10 @@ async function handler(req, res) {
     };
 
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.hvrekjg.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
     try {
-       client = await MongoClient.connect(
-        "mongodb+srv://tarekanwer:etpBXzOb7NuOz2Au@cluster0.hvrekjg.mongodb.net/my-site?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({
         message: "could not connect to database",
